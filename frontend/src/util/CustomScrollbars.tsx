eb046bfba4c5f08ea4012d162b719201b328c5fc
plat123456789 <plat123456789@qq.com>
2018-08-26 23:49:12 +0800

    fixing scrolling, delete unuse code

---
+++ frontend/src/util/CustomScrollbars.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+// tslint:disable-next-line:no-var-requires
+const Scrollbars = require("react-custom-scrollbars").Scrollbars;
+
+export interface ICustomScrollbarsProps {
+  [key: string]: any;
+}
+
+const CustomScrollbars: React.SFC<ICustomScrollbarsProps> = (props: any) => {
+  return (
+    <Scrollbars
+      {...props}
+      autoHide={true}
+      // tslint:disable-next-line:no-shadowed-variable jsx-no-lambda
+      renderTrackHorizontal={(props: any) => (
+        <div
+          {...props}
+          style={{ display: "none" }}
+          className="track-horizontal"
+        />
+      )}
+    />
+  );
+};
+
+export default CustomScrollbars;
