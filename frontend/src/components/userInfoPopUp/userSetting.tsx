4ee82d77932e70ddc29a5d9bbc9de86d0b972103
plat123456789 <plat123456789@qq.com>
2018-09-09 20:36:45 +0800

    mirror change

--- frontend/src/components/userInfoPopUp/userSetting.tsx
+++ frontend/src/components/userInfoPopUp/userSetting.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import ContainerHeader from "../common/containerHeader";
 
 // export interface IUserProfileProps {
 
@@ -10,7 +11,11 @@ import * as React from "react";
 
 class UserSetting extends React.Component {
   public render() {
-    return <h1>User Setting</h1>;
+    return (
+      <div className="animated slideInUpTiny animation-duration-3">
+        <ContainerHeader title="Account setting" />
+      </div>
+    );
   }
 }
 
