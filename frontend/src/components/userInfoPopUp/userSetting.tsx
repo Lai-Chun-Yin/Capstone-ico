86cc8266c8908d756ad95119c0f7a32b50250fd0
Lai-Chun-Yin <cylai0723@gmail.com>
2018-09-11 10:47:53 +0800

    image editor on userSetting

--- frontend/src/components/userInfoPopUp/userSetting.tsx
+++ frontend/src/components/userInfoPopUp/userSetting.tsx
@@ -1,16 +1,32 @@
 import { Button } from "@material-ui/core";
 import * as React from "react";
+import AvatarEditor from "react-avatar-editor";
+import Dropzone from 'react-dropzone';
 import ContainerHeader from "../common/containerHeader";
 
 // export interface IUserProfileProps {
 
 // }
 
-// export interface IUserProfileState {
+export interface IUserSettingState {
+  file: File | null;
+  editedImage: File | null;
+  imageScale: number;
+}
 
-// }
+// const sliderStyles = {
+//     width: 300
+// };
 
-class UserSetting extends React.Component {
+class UserSetting extends React.Component<any, IUserSettingState> {
+  constructor(props:any) {
+    super(props);
+    this.state = {
+      file: null,
+      editedImage: null,
+      imageScale: 100
+    }
+  }
   public render() {
     return (
       <div className="animated slideInUpTiny animation-duration-3">
@@ -45,6 +61,33 @@ class UserSetting extends React.Component {
                     name="lastname"
                   />
                   <br />
+                  <h3>Please drop image into below area for changing profile picture.</h3>
+                  <div>
+                    <Dropzone
+                      multiple={false}
+                      accept="image/*"
+                      disableClick={true}
+                      style={{
+                        width: "350px",
+                        height: "350px"
+                      }}
+                      // tslint:disable-next-line:jsx-no-bind
+                      onDrop={this.onImageDrop.bind(this)}>
+                      <AvatarEditor
+                        width={250}
+                        height={250}
+                        border={50}
+                        scale={this.state.imageScale/100 }
+                        image={this.state.file || ""}
+                        // tslint:disable-next-line:jsx-no-bind
+                        ref={this.setEditorRef.bind(this)}
+                      />
+                    </Dropzone>
+                  </div>
+                  <div>
+                    <p>Scale:</p>
+                    <input type="range" min="50" max="150" value={this.state.imageScale} onChange={this.scaleSliderHandler} />
+                  </div>
                   <Button
                     className="jr-btn bg-primary text-white m-2"
                     type="submit"
@@ -59,6 +102,44 @@ class UserSetting extends React.Component {
         </div>
       </div>
     );
+  }
+
+  // private onFileChange = (event: any) => {
+  //   this.setState({ file: event.target.files[0] });
+  // }
+
+  private onImageDrop(files: File[]) {
+    this.setState({
+      file: files[0]
+    });
+  }
+
+  private setEditorRef(editor: any) {
+    if (editor) {
+      const canvas = editor.getImageScaledToCanvas();
+      const fileName = this.state.file ? this.state.file.name : "file.png";
+      this.getCanvasBlob(canvas)
+        .then((blob: Blob) => {
+          const editedImage = new File([blob], fileName);
+          this.setState({
+            editedImage
+          })
+        })
+    }
+  }
+
+  private getCanvasBlob(canvas: HTMLCanvasElement) {
+    return new Promise((resolve, reject)=> {
+      canvas.toBlob((blob: Blob)=> {
+        resolve(blob);
+      });
+    });
+  }
+
+  private scaleSliderHandler = (e: any) => {
+    this.setState({
+      imageScale: +e.target.value
+    })
   }
 }
 
